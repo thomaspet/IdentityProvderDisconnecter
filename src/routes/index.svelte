6475dac8832dc7dd8189bbc0b922d66ea3a26bd9
Thomas Petersson <thomaspet@unimicro.no>
2021-10-26 11:44:04 +0200

    init

---
+++ src/routes/index.svelte
@@ -0,0 +1,40 @@
+<script lang="ts">
+    let token = "";
+    let promise = null;
+    function send() {
+        if (token?.toLowerCase().startsWith("bearer")) {
+            token = token.substring("bearer".length).trim()
+        }
+        promise = fetch("https://login.dnbregnskap.dnb.no/External/Disconnect?scheme=bankid", {method: "POST", headers: {Authorization: `Bearer ${token}`}});
+    }
+
+    function formatToken(e) {
+        const data = e.target.value;
+
+        if (data?.toLowerCase().startsWith("bearer")) {
+            token = data.substring("bearer".length).trim()
+        }
+
+    }
+
+</script>
+
+<h1>Fyll inn token og trykk send</h1>
+
+
+<textarea bind:value={token} on:input={formatToken} placeholder="Token her"></textarea>
+<br>
+<button on:click={send}>Send</button>
+
+{#if promise}
+    <br>
+    {#await promise then res}
+        {#if res.status === 404}
+            Bruker finnes ikke eller er allerede koblet av
+        {:else}
+            Success...
+        {/if}
+    {:catch err}
+        {err}
+    {/await}
+{/if}
